61b969ba3ef65121141c2d75f3a638365e71514a
eddybogotacangrejo <eddybogota@gmail.com>
2020-10-07 21:00:42 -0500

    segundo commit

--- validacion.php
+++ validacion.php
@@ -1,20 +1,20 @@
 <?php
-if(($_POST['n1']) != "" and ($_POST['n1']) != "" )){
+if(($_POST['n1']) != "" and ($_POST['n2']) != "" )){
   if($_POST['operador'] == "+"){
       print($resultado = $_POST['n1'] + $_POST['n1']);
-      print('<br><a href=calc.html">volver</a>');
+      print('<br><a href=operaciones_funciones.php">volver</a>');
   }
   elseif($_POST['operador'] == "-"){
     print($resultado = $_POST['n1'] - $_POST['n1']);
-    print('<br><a href=calc.html">volver</a>');
+    print('<br><a href=operaciones_funciones.php">volver</a>');
   }
   elseif($_POST['operador'] == "*"){
     print($resultado = $_POST['n1'] * $_POST['n1']);
-    print('<br><a href=calc.html">volver</a>');
+    print('<br><a href=operaciones_funciones.php">volver</a>');
   }
   elseif($_POST['operador'] == "/"){
     print($resultado = $_POST['n1'] / $_POST['n1']);
-    print('<br><a href=calc.html">volver</a>');
+    print('<br><a href=operaciones_funciones.php">volver</a>');
   }
 }
 ?>
