492be6744c53631c4fd7d36699a71907bcbe4011
eddybogotacangrejo <eddybogota@gmail.com>
2020-10-10 08:19:24 -0500

    cuarto commit

--- validacion.php
+++ validacion.php
@@ -16,5 +16,9 @@ if(($_POST['n1'] != "") and ($_POST['n2'] != "" )){
     print($resultado = $_POST['n1'] / $_POST['n2']);
     print('<br><a href=FUNCIONES_CON_PHP.php>volver</a>');
   }
+  elseif($_POST['operador'] == "p"){
+    print($resultado = $_POST['n1'] * $_POST['n1']);
+    print('<br><a href=FUNCIONES_CON_PHP.php>volver</a>');
+  }  
 }
 ?>
